4469972f260a4218b53bfbaf1410c90e2c563829
Adelaaide Grey <dendygrey@gmail.com>
2023-03-02 09:49:58 +0100

    Delete src directory

--- src/db.js
+++
@@ -1,8 +0,0 @@
-import { Client } from 'pg';
-
-const client = new Client ({
-    connectionString: "postgres:Qc6NlxMq0Eewv4GbcQVx@containers-us-west-108.railway.app:7623/railway",
-    ssl: true 
-});
-
-export default client;

8c515170d54b8a2ab2bfa29b8d843ae228482b89
Adelaaide Grey <dendygrey@gmail.com>
2023-03-03 10:00:30 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch('http://localhost:3000/imageurl', {
+      fetch('https://jade-faithful-skunk.cyclic.app/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -74,7 +74,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://jade-faithful-skunk.cyclic.app/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
